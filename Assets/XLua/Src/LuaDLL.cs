1223734154d5eb0dc04e107fa964f8d879305821
moyu <403231700@qq.com>
2020-07-02 19:06:32 +0800

    [lua-protobuf] 添加加载pb.dll代码; 编译windows xlua.dll

--- Assets/XLua/Src/LuaDLL.cs
+++ Assets/XLua/Src/LuaDLL.cs
@@ -580,6 +580,14 @@ namespace XLua.LuaDLL
         [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr xlua_gl(IntPtr L);
 
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int luaopen_pb(System.IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadLuaProtobuf(System.IntPtr L)
+        {
+            return luaopen_pb(L);
+        }
 #if GEN_CODE_MINIMIZE
         [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
         public static extern void xlua_set_csharp_wrapper_caller(IntPtr wrapper);
